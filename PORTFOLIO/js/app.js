04b8ed3890204382e99e4eadcf3e8a960370f9c3
nilan92 <50830508+nilan92@users.noreply.github.com>
2021-09-09 22:40:58 +0530

    major portfolio update and homepage tweaks.. MAJOR UPDATE

--- PORTFOLIO/js/app.js
+++ PORTFOLIO/js/app.js
@@ -1,8 +1,56 @@
+// page first impression animation 
 gsap.timeline()
 
 .to(".text", {y:"0", duration: 1, stagger: 0.2})
 .to(".slider", {y:"-100%", duration: 1.5, delay: 0.5})
 .to(".intro", {y:"-100%", duration: 1}, "-=1")
-.from("header, main", {opacity: 0, duration:1})
+.from("header", {opacity: 0, duration:1})
 .from(".big-text", {duration:1, opacity: 0}, "-=1")
 
+// lightcase lightbox java stuff
+jQuery(document).ready(function($) { 
+    $('a[data-rel^=lightcase]').lightcase();
+  });
+
+// text typing animation 
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["More", "Creative", "Elegant", "Happy"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
